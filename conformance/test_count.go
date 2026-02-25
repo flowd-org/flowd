818f5e12c1d9d3f8df2879370adba579ae371f86
Didac Oliveira <me@didacog.eu>
2026-02-25 15:51:11 +0100

    Remove accidental conformance debug helper source file (issue #224)

--- conformance/test_count.go
+++ conformance/test_count.go
@@ -1,11 +1,1 @@
-package main
 
-import "fmt"
-
-func main() {
-	s := `{"header": "Authorization: Bearer abc123"}`
-	fmt.Println("Length:", len(s))
-	for i, c := range s {
-		fmt.Printf("%d: %q\n", i, c)
-	}
-}
