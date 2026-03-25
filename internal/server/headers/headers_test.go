b8d7c59c465cdff71bec711dbde605979ee977fd
DAD Harness <dad-harness@local.invalid>
2026-03-25 10:15:14 +0000

    [PR-339] Re-fix live golangci-lint failure from latest imported run (headers_test.go) (issue #377)

--- internal/server/headers/headers_test.go
+++ internal/server/headers/headers_test.go
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-package headers
+package headers_test
 
 import (
 	"testing"
+
+	"github.com/flowd-org/flowd/internal/server/headers"
 )
 
 func TestDiscoveryErrors(t *testing.T) {
-	if DiscoveryErrors != "X-Flowd-Discovery-Errors" {
-		t.Errorf("expected DiscoveryErrors to be 'X-Flowd-Discovery-Errors', got '%s'", DiscoveryErrors)
+	if headers.DiscoveryErrors != "X-Flowd-Discovery-Errors" {
+		t.Errorf("expected DiscoveryErrors to be 'X-Flowd-Discovery-Errors', got '%s'", headers.DiscoveryErrors)
 	}
 }
